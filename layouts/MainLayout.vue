fbc071dba8bc6ad879ba31802e7414e19227fe0d
Hiba El Echi <70015731+Hiba-El-Echi@users.noreply.github.com>
2023-10-11 11:41:13 +0100

    account dropdown

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -27,13 +27,38 @@
         <li
           @mouseenter="isAccountMenu = true"
           @mouseleave="isAccountMenu = false"
-          class="relative"
-        ></li>
+          class="relative flex items-center px-2.5 hover:text-green-400 h-full cursor-pointer"
+          :class="
+            isAccountMenu
+              ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]'
+              : 'border border-[#FAFAFA]'
+          "
+        >
+          <Icon name="ph:user-thin" size="17" />
+          Account
+          <Icon name="mdi:chevron-down" size="15" class="ml-5" />
+          <div id="AccountMenu" v-if="isAccountMenu" class="absolute bg-white w-[214px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b">
+            <div v-if="true">
+                <div class="text-semibold text-[15px] my-4 px-3">Welcome !</div>
+                <div class="flex items-center gap-1 px-3 mb-3 ">
+                    <NuxtLink to="/auth" class="bg-green-400 text-center w-full text-[16px] rounded-sm text-white font-semi-bold p-2">
+                      Login / Register
+                    </NuxtLink>
+                </div>
+
+            </div>
+          </div>
+        </li>
       </ul>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+ let isAccountMenu = ref(false)
+</script>
 
 <style></style>
+ 
