8270f4056f7e0d456dec39f57fa597e12f06c1b8
Hiba El Echi <70015731+Hiba-El-Echi@users.noreply.github.com>
2023-10-12 21:04:16 +0100

    products

---
+++ components/Product.vue
@@ -0,0 +1,10 @@
+import { Product } from '#build/components';
+<template>
+  <div
+    :id="`ProductComponent${product.id}`"
+    class="bg-white inline-block rounded hover:shadow-[0_0_10px_3px_rgba(0,0,0,0.15)] cursor-pointer"
+  ></div>
+</template>
+<script setup>
+const props = defineProps([product])
+</script>
