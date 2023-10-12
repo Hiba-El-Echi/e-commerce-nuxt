76c895e3a6c76caf08c72301ef38439b65d6d0c9
Hiba El Echi <70015731+Hiba-El-Echi@users.noreply.github.com>
2023-10-12 20:00:26 +0100

    main layout footer finished

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -89,19 +89,78 @@
                 class="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none"
                 placeholder="kitchen accessories"
                 type="text"
+                v-model="searchItem"
               />
+              <Icon
+                v-if="isSearching"
+                name="eos-icons:loading"
+                size="25"
+                class="mr-2"
+              />
+              <button
+                class="flex items-center h-[100%] p-1.5 px-2 bg-green-400"
+              >
+                <Icon name="ph:magnifying-glass" size="20" color="#ffffff" />
+              </button>
+            </div>
+            <div class="absolute bg-white max-w-[700px] h-auto w-full">
+              <div class="p-1">
+                <NuxtLink
+                  to="`/item/1`"
+                  class="flex items-center justify-between w-full cursor-pointer hover:bg-gray-100"
+                >
+                  <div class="flex items-center">
+                    <img class="rounded-md" width="40" src="" />
+                    <div class="truncate ml-2">Test</div>
+                  </div>
+                  <div class="truncate">$99.55</div>
+                </NuxtLink>
+              </div>
             </div>
           </div>
         </div>
+        <NuxtLink to="/shoppingcart" class="flex items-center">
+          <button
+            class="relative md:block hidden"
+            @mouseenter="isCartHover = true"
+            @mouseleave="isCartHover = false"
+          >
+            <span
+              class="absolute flex items-center justify-center -right-[3px] top-0 bg-green-400 h-[17px] min-w-[17px] text-xs text-white px-0.5 rounded-full"
+            >
+              0
+            </span>
+            <div class="min-w-[40px]">
+              <Icon
+                name="ph:shopping-cart-simple-light"
+                size="33"
+                :color="isCartHover ? '#4ade80' : ''"
+              />
+            </div>
+          </button>
+        </NuxtLink>
+        <button
+          @click="userStore.isMenuOverlay = true"
+          class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200"
+        >
+          <Icon name="radix-icons:hamburger-menu" size="33" />
+        </button>
       </div>
     </div>
   </div>
+  <div class="lg:pt-[150px] md:pt-[130px] pt-[80px]" />
+  <slot />
+   <Footer v-if="!userStore.isLoading"/>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore()
 
-let isAccountMenu = ref(false)
+let isAccountMenu = ref(false);
+let searchItem = ref("");
+let isSearching = ref(false);
+let isCartHover = ref(false);
 </script>
 
 <style></style>
