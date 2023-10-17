7ed62c54fac79d92e52fb494226ab505edc2e443
Hiba El Echi <70015731+Hiba-El-Echi@users.noreply.github.com>
2023-10-17 12:12:35 +0100

    product item layout

--- pages/index.vue
+++ pages/index.vue
@@ -44,6 +44,48 @@ const products = [
     url: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
     price: 99,
   },
+  {
+    id: 4,
+    title: "Tile 1",
+    description: "descriptionnnn ",
+    url: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    price: 99,
+  },
+  {
+    id: 4,
+    title: "Tile 1",
+    description: "descriptionnnn ",
+    url: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    price: 99,
+  },
+  {
+    id: 4,
+    title: "Tile 1",
+    description: "descriptionnnn ",
+    url: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    price: 99,
+  },
+  {
+    id: 4,
+    title: "Tile 1",
+    description: "descriptionnnn ",
+    url: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    price: 99,
+  },
+  {
+    id: 4,
+    title: "Tile 1",
+    description: "descriptionnnn ",
+    url: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    price: 99,
+  },
+  {
+    id: 4,
+    title: "Tile 1",
+    description: "descriptionnnn ",
+    url: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    price: 99,
+  },
 ];
 </script>
 
---
+++ pages/item/[id].vue
@@ -0,0 +1,45 @@
+<template>
+  <MainLayout>
+    <div id="ItemPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div class="md:flex gap-4 justify-between mx-auto w-full">
+        <div class="md:w-[40%]">
+          <img
+            class="rounded-lg object-fit"
+            src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+          />
+          <div
+            v-if="images[0] !== ''"
+            class="flex items-center justify-center mt-2"
+          >
+            <div v-for="image in images">
+              <img
+                @mouseover="currentImage = image"
+                @click="currentImage = image"
+                width="70"
+                class="rounded-md object-fit border-[3px] cursor-pointer"
+                :class="currentImage === image ? 'border-[#FF5353]' : ''"
+                :src="image"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </MainLayout>
+</template>
+<script setup>
+import MainLayout from "~/layouts/MainLayout.vue";
+
+watchEffect(() => {
+  images.value[0] = "https://picsum.photos/id/77/800/800"
+});
+
+const images = ref([
+  "",
+  "https://picsum.photos/id/212/800/800",
+  "https://picsum.photos/id/233/800/800",
+  "https://picsum.photos/id/165/800/800",
+  "https://picsum.photos/id/99/800/800",
+  "https://picsum.photos/id/144/800/800",
+]);
+</script>
