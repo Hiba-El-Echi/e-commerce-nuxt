69c4a34e77ba8ec72693c3482b4d00de9588c98b
Hiba El Echi <70015731+Hiba-El-Echi@users.noreply.github.com>
2023-10-18 17:46:49 +0100

    up

--- components/CartItem.vue
+++ components/CartItem.vue
@@ -1,23 +1,26 @@
 <template>
- <div class="flex justify-start my-2">
-  <div class="my-auto">
-            <div 
-                @mouseenter="isHover = true"
-                @mouseleave="isHover = false"
-                class="flex items-center justify-start p-0.5 cursor-pointer"
-            >
-                <div 
-                    @click="isSelected = !isSelected"
-                    class=" flex items-center justify-center h-[20px] w-[20px] rounded-full border mr-5 ml-2"
-                    :class="[
-                        isHover ? 'border-[#FD374F]' : 'border-gray-300',
-                        isSelected ? 'bg-[#FD374F]' : ''
-                    ]"
-                >
-                    <div class="h-[8px] w-[8px] rounded-full bg-white" />
-                </div>
-            </div>
+  <div class="flex justify-start my-2">
+    <div class="my-auto">
+      <div
+        @mouseenter="isHover = true"
+        @mouseleave="isHover = false"
+        class="flex items-center justify-start p-0.5 cursor-pointer"
+      >
+        <div
+          @click="isSelected = !isSelected"
+          class="flex items-center justify-center h-[20px] w-[20px] rounded-full border mr-5 ml-2"
+          :class="[
+            isHover ? 'border-[#4ade80]' : 'border-gray-300',
+            isSelected ? 'bg-[#4ade80]' : '',
+          ]"
+        >
+          <div class="h-[8px] w-[8px] rounded-full bg-white" />
         </div>
-
- </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script setup>
+let isHover = ref(false)
+let isSelected = ref(false)
+</script>
