1aaf4bc46412810fe0ba3678db6f114597610aed
Hiba El Echi <70015731+Hiba-El-Echi@users.noreply.github.com>
2023-11-06 18:27:03 +0100

    orders and checkout update

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,185 +1,169 @@
 <template>
-    <MainLayout>
-        <div id="CheckoutPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-  
-            <div class="md:flex gap-4 justify-between mx-auto w-full">
-                <div class="md:w-[65%]">
-                    <div class="bg-white rounded-lg p-4">
+  <MainLayout>
+    <div id="CheckoutPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div class="md:flex gap-4 justify-between mx-auto w-full">
+        <div class="md:w-[65%]">
+          <div class="bg-white rounded-lg p-4">
+            <div class="text-xl font-semibold mb-2">Shipping Address</div>
+            <div v-if="currentAddress && currentAddress.data">
+              <NuxtLink
+                to="/address"
+                class="flex items-center pb-2 text-blue-500 hover:text-red-400"
+              >
+                <Icon name="mdi:plus" size="18" class="mr-2" />
+                Update Address
+              </NuxtLink>
 
-                        <div class="text-xl font-semibold mb-2">Shipping Address</div>
-
-                        <div v-if="currentAddress && currentAddress.data">
-                            <NuxtLink 
-                                to="/address"
-                                class="flex items-center pb-2 text-blue-500 hover:text-red-400"
-                            >
-                                <Icon name="mdi:plus" size="18" class="mr-2"/>
-                                Update Address
-                            </NuxtLink>
-
-                            <div class="pt-2 border-t">
-                                <div class="underline pb-1">Delivery Address</div>
-                                <ul class="text-xs">
-                                    <li class="flex items-center gap-2">
-                                        <div>Contact name:</div> 
-                                        <div class="font-bold">{{ currentAddress.data.name }}</div>
-                                    </li>
-                                    <li class="flex items-center gap-2">
-                                        <div>Address:</div> 
-                                        <div class="font-bold">{{ currentAddress.data.address }}</div>
-                                    </li>
-                                    <li class="flex items-center gap-2">
-                                        <div>Zip Code:</div> 
-                                        <div class="font-bold">{{ currentAddress.data.zipcode }}</div>
-                                    </li>
-                                    <li class="flex items-center gap-2">
-                                        <div>City:</div> 
-                                        <div class="font-bold">{{ currentAddress.data.city }}</div>
-                                    </li>
-                                    <li class="flex items-center gap-2">
-                                        <div>Country:</div> 
-                                        <div class="font-bold">{{ currentAddress.data.country }}</div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <NuxtLink 
-                            v-else
-                            to="/address"
-                            class="flex items-center text-blue-500 hover:text-red-400"
-                        >
-                            <Icon name="mdi:plus" size="18" class="mr-2"/>
-                            Add New Address
-                        </NuxtLink>
+              <div class="pt-2 border-t">
+                <div class="underline pb-1">Delivery Address</div>
+                <ul class="text-xs">
+                  <li class="flex items-center gap-2">
+                    <div>Contact name:</div>
+                    <div class="font-bold">{{ currentAddress.data.name }}</div>
+                  </li>
+                  <li class="flex items-center gap-2">
+                    <div>Address:</div>
+                    <div class="font-bold">
+                      {{ currentAddress.data.address }}
                     </div>
-
-                    <div id="Items" class="bg-white rounded-lg p-4 mt-4">
-                        <div v-for="product in userStore.checkout">
-                            <CheckoutItem :product="product" />
-                        </div>
+                  </li>
+                  <li class="flex items-center gap-2">
+                    <div>Zip Code:</div>
+                    <div class="font-bold">
+                      {{ currentAddress.data.zipcode }}
                     </div>
-                </div>
-
-                <div class="md:hidden block my-4"/>
-                <div class="md:w-[35%]">
-                    <div id="PlaceOrder" class="bg-white rounded-lg p-4">
-
-                        <div class="text-2xl font-extrabold mb-2">Summary</div>
-
-                        <div class="flex items-center justify-between my-4">
-                            <div class="">Total Shipping</div>
-                            <div class="">Free</div>
-                        </div>
-
-                        <div class="border-t" />
-
-                        <div class="flex items-center justify-between my-4">
-                            <div class="font-semibold">Total</div>
-                            <div class="text-2xl font-semibold">
-                                $ <span class="font-extrabold">{{ total / 100 }}</span>
-                            </div>
-                        </div>
-
-                        <form @submit.prevent="pay()">
-                            <div 
-                                class="border border-gray-500 p-2 rounded-sm" 
-                                id="card-element" 
-                            />
-
-                            <p 
-                                id="card-error" 
-                                role="alert" 
-                                class="text-red-700 text-center font-semibold" 
-                            />
-
-                            <button 
-                                :disabled="isProcessing"
-                                type="submit"
-                                class="
-                                mt-4
-                                    bg-gradient-to-r 
-                                  from-[#FE630C] 
-                                  to-[#FF3200]
-                                    w-full 
-                                    text-white 
-                                    text-[21px] 
-                                    font-semibold 
-                                    p-1.5 
-                                    rounded-full
-                                "
-                                :class="isProcessing ? 'opacity-70' : 'opacity-100'"
-                            >
-                                <Icon v-if="isProcessing" name="eos-icons:loading" />
-                                <div v-else>Place order</div>
-                            </button>
-                        </form>
-
-                    </div>
-
-                    <div class="bg-white rounded-lg p-4 mt-4">
-                        <div class="text-lg font-semibold mb-2 mt-2">AliExpress</div>
-                        <p class="my-2">
-                            AliExpress keeps your information and payment safe
-                        </p>
-
-                    </div>
-                </div>
+                  </li>
+                  <li class="flex items-center gap-2">
+                    <div>City:</div>
+                    <div class="font-bold">{{ currentAddress.data.city }}</div>
+                  </li>
+                  <li class="flex items-center gap-2">
+                    <div>Country:</div>
+                    <div class="font-bold">{{ currentAddress.data.city }}</div>
+                  </li>
+                </ul>
+              </div>
             </div>
+            <NuxtLink
+              v-else
+              to="/address"
+              class="flex items-center text-blue-500 hover:text-red-400"
+            >
+              <Icon name="mdi:plus" size="18" class="mr-2" />
+              Add New Address
+            </NuxtLink>
+          </div>
+          <div id="Items" class="bg-white rounded-lg p-4 mt-4">
+            <div v-for="product in userStore.checkout">
+              <CheckoutItem :product="product" />
+            </div>
+          </div>
         </div>
-    </MainLayout>
+        <div class="md:hidden block my-4" />
+        <div class="md:w-[35%]">
+          <div id="PlaceOrder" class="bg-white rounded-lg p-4">
+            <div class="text-2xl font-extrabold mb-2">Summary</div>
+
+            <div class="flex items-center justify-between my-4">
+              <div class="">Total Shipping</div>
+              <div class="">Free</div>
+            </div>
+
+            <div class="border-t" />
+
+            <div class="flex items-center justify-between my-4">
+              <div class="font-semibold">Total</div>
+              <div class="text-2xl font-semibold">
+                $ <span class="font-extrabold">{{ total / 100 }}</span>
+              </div>
+            </div>
+
+            <form @submit.prevent="pay()">
+              <div
+                class="border border-gray-500 p-2 rounded-sm"
+                id="card-element"
+              />
+
+              <p
+                id="card-error"
+                role="alert"
+                class="text-red-700 text-center font-semibold"
+              />
+
+              <button
+                :disabled="isProcessing"
+                type="submit"
+                class="mt-4 bg-gradient-to-r from-[#FE630C] to-[#FF3200] w-full text-white text-[21px] font-semibold p-1.5 rounded-full"
+                :class="isProcessing ? 'opacity-70' : 'opacity-100'"
+              >
+                <Icon v-if="isProcessing" name="eos-icons:loading" />
+                <div v-else>Place order</div>
+              </button>
+            </form>
+          </div>
+          <div class="bg-white rounded-lg p-4 mt-4">
+            <div class="text-lg font-semibold mb-2 mt-2">Express Shop</div>
+            <p class="my-2">
+              This Website keeps your information and payment safe
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </MainLayout>
 </template>
-
-
 <script setup>
-import MainLayout from '~/layouts/MainLayout.vue';
-import { useUserStore } from '~/stores/user';
-const userStore = useUserStore()
-const user = useSupabaseUser()
-const route = useRoute()
+import MainLayout from "~/layouts/MainLayout.vue";
+import { useUserStore } from "~/stores/user";
+const userStore = useUserStore();
+const user = useSupabaseUser();
+const route = useRoute();
+let selectedArray = ref([]);
 
-definePageMeta({ middleware: "auth" })
-
-let stripe = null
-let elements = null
-let card = null
-let form = null
-let total = ref(0)
-let clientSecret = null
-let currentAddress = ref(null)
-let isProcessing = ref(false)
+let stripe = null;
+let elements = null;
+let card = null;
+let form = null;
+let total = ref(0);
+let clientSecret = null;
+let currentAddress = ref(null);
+let isProcessing = ref(false);
 
 onBeforeMount(async () => {
-    if (userStore.checkout.length < 1) {
-        return navigateTo('/shoppingcart')
-    }
+  if (userStore.checkout.length < 1) {
+    return navigateTo("/shoppingcart");
+  }
 
-    total.value = 0.00
-    if (user.value) {
-        currentAddress.value = await useFetch(`/api/prisma/get-address-by-user/${user.value.id}`)
-        setTimeout(() => userStore.isLoading = false, 200)
-    }
-})
+  total.value = 0.0;
+  if (user.value) {
+    currentAddress.value = await useFetch(
+      `/api/prisma/get-address-by-user/${user.value.id}`
+    );
+    setTimeout(() => (userStore.isLoading = false), 200);
+  }
+});
 
 watchEffect(() => {
-    if (route.fullPath == '/checkout' && !user.value) {
-        return navigateTo('/auth')
-    }
-})
+  if (route.fullPath == "/checkout" && !user.value) {
+    return navigateTo("/auth");
+  }
+});
 
-onMounted(async () => {
-    isProcessing.value = true
+onMounted(() => {
+  isProcessing.value = true;
+  userStore.checkout.forEach((item) => {
+    total.value += item.price;
+  });
+});
 
-    userStore.checkout.forEach(item => {
-        total.value += item.price
-    })
-})
-
-watch(() => total.value, () => {
+watch(
+  () => total.value,
+  () => {
     if (total.value > 0) {
-        stripeInit()
+      stripeInit();
     }
-})
+  }
+);
 
 const stripeInit = async () => {
     const runtimeConfig = useRuntimeConfig()
@@ -209,7 +193,7 @@ const stripeInit = async () => {
         style: style 
     });
 
-    // Stripe injects an iframe into the DOM
+     
     card.mount("#card-element");
     card.on("change", function (event) {
         // Disable the Pay button if there are no card details in the Element
@@ -219,7 +203,6 @@ const stripeInit = async () => {
 
     isProcessing.value = false
 }
-
 const pay = async () => {
     if (currentAddress.value && currentAddress.value.data == '') {
         showError('Please add shipping address')
--- stores/products.js
+++ stores/products.js
@@ -12,7 +12,6 @@ export const useProductsStore = defineStore("products", {
         const response = await fetch("/api/prisma/get-all-products");
         const data = await response.json();
         this.products = data;
-        console.log(this.products, "products in store");
       } catch (error) {
         console.log(error, "errorr fetching products");
         return error;
